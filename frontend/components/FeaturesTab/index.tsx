c3d022cb6cc917f79adfb98648880f7b7e347442
shishir-insane <shishir.insane@gmail.com>
2024-12-07 19:00:55 +0100

    Update feature titles and button labels for clarity and relevance

--- frontend/components/FeaturesTab/index.tsx
+++ frontend/components/FeaturesTab/index.tsx
@@ -63,7 +63,7 @@ const FeaturesTab = () => {
               </div>
               <div className="md:w-3/5 lg:w-auto">
                 <button className="text-sm font-medium text-black dark:text-white xl:text-regular">
-                  Clean User Interface
+                  Dashboard
                 </button>
               </div>
             </div>
@@ -82,7 +82,7 @@ const FeaturesTab = () => {
               </div>
               <div className="md:w-3/5 lg:w-auto">
                 <button className="text-sm font-medium text-black dark:text-white xl:text-regular">
-                  Essential Business Pages
+                  Analytics
                 </button>
               </div>
             </div>
@@ -101,7 +101,7 @@ const FeaturesTab = () => {
               </div>
               <div className="md:w-3/5 lg:w-auto">
                 <button className="text-sm font-medium text-black dark:text-white xl:text-regular">
-                  Fully Functional Integrations
+                  Integrations
                 </button>
               </div>
             </div>
